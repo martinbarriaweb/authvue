19aaa03c8eda2dd7e8e457053e12f2de0757c112
Martin Barria Joost <martinjoostw@gmail.com>
2020-09-24 08:59:12 -0600

    feat register path

---
+++ src/views/Register.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1>User register</h1>
+    <form>
+      <input v-model="email" type="text" placeholder="Ingrese email" />
+      <input v-model="password" type="text" placeholder="Ingrese password" />
+      <input
+        v-model="confirm_password"
+        type="text"
+        placeholder="Confirme password"
+      />
+      <button type="submit">Registrar usuario</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {
+    return {
+      email: "",
+      password: "",
+      confirm_password: "",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
